3a8b9ff3f0b8ef9024315678e095d05f0bfb96ab
surendra_vidiyala <vidiyala.surendra@gmail.com>
2017-06-08 16:15:56 -0500

    created comment schema in dishes

--- models/dishes.js
+++ models/dishes.js
@@ -5,19 +5,19 @@ require('mongoose-currency').loadType(mongoose);
 var Currency = mongoose.Types.Currency;
 
 var commentSchema = new Schema({
-    rating:  {
+    rating: {
         type: Number,
         min: 1,
         max: 5,
         required: true
     },
-    comment:  {
+    comment: {
         type: String,
         required: true
     },
-    author:  {
-        type: String,
-        required: true
+    postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 }, {
     timestamps: true
@@ -50,7 +50,7 @@ var dishSchema = new Schema({
         type: String,
         required: true
     },
-    comments:[commentSchema]
+    comments: [commentSchema]
 }, {
     timestamps: true
 });
